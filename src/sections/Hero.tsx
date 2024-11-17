8d84f69e9c67546ad581c4aa88869efd3e0fb32f
Suryansh Chourasia <suryanshchourasia12@gmail.com>
2024-11-17 20:03:13 +0530

    first commit

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,10 @@
+export default function Hero() {
+  return (
+    <section>
+      <div className="container">
+        <div>$7.5M seed round raised</div>
+        <h1>Impactful design, created effortlessly</h1>
+      </div>
+    </section>
+  );
+}
