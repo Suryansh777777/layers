4d7e9dd4f76b85d7c9fe35d7bdda7a81e75f6acf
Suryansh Chourasia <suryanshchourasia12@gmail.com>
2025-02-07 16:43:20 +0530

    Add LogoTicker component and update Hero section for responsive design

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -7,16 +7,16 @@ export default function Hero() {
   return (
     <section className="py-24 overflow-x-clip">
       <div className="container relative">
-        <div className="absolute -left-32 top-16">
+        <div className="absolute -left-32 top-16 hidden lg:block">
           <Image src={designExample1Image} alt="Design Example 1" />
         </div>
-        <div className="absolute -right-64 -top-16">
+        <div className="absolute -right-64 -top-16 hidden lg:block">
           <Image src={designExample2Image} alt="Design Example 1" />
         </div>
-        <div className="absolute left-56 top-96">
+        <div className="absolute left-56 top-96 hidden lg:block">
           <Pointer name="Andrea" />
         </div>
-        <div className="absolute right-80 -top-4">
+        <div className="absolute right-80 -top-4 hidden lg:block">
           <Pointer name="Bryan" color="red" />
         </div>
         <div className="flex justify-center">
