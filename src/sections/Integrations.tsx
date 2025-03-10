d8dd5c5e6773fd05b9d9f9fed5925bc4aad0e264
Suryansh Chourasia <suryanshchourasia12@gmail.com>
2025-03-11 00:21:59 +0530

    Add reverse animation effect to IntegrationsColumn component

--- src/sections/Integrations.tsx
+++ src/sections/Integrations.tsx
@@ -61,6 +61,7 @@ export default function Integrations() {
               <IntegrationsColumn integrations={integrations} />
               <IntegrationsColumn
                 integrations={integrations.slice().reverse()}
+                reverse
                 className="hidden md:flex"
               />
             </div>
